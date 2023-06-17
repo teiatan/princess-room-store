5fcea1c16394a00a51b94145e0cb8ec59bab6cd8
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-17 15:33:46 +0300

    delete unneccessary

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { AppContainer } from "./App.styled";
+
+function App() {
+  return (
+    <AppContainer>
+      <h1>Princess Room Store</h1>
+    </AppContainer>
+  );
+}
+
+export default App;
